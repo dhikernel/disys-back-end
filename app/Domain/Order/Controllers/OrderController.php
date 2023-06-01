8af1a8ca5342e8199db7e7015d5036104d1d2849
Diego Pereira <san_diego19@hotmail.com>
2023-05-31 23:50:26 -0300

    feat: order module implementation

---
+++ app/Domain/Order/Controllers/OrderController.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Order\Controllers;
+use App\Domain\Order\Repositories\OrderRepository;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+class OrderController extends Controller
+{
+    protected $repository;
+
+    protected array $validators = [
+        'name' => 'required|string|max:255',
+        'price' => 'required|regex:/^\d{1,13}(\.\d{1,4})?$/',
+        'photo' => 'required|string',
+    ];
+
+    /**
+     * @param OrderRepository $repository
+     */
+    public function __construct(OrderRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index(Request $request)
+    {
+        return parent::index($request);
+    }
+
+
+    public function store(Request $request)
+    {
+        return parent::store($request);
+    }
+
+    public function update(Request $request, int $id)
+    {
+        return parent::update($request, $id);
+    }
+
+    public function destroy(int $id)
+    {
+        return parent::destroy($id);
+    }
+
+    public function show($id)
+    {
+        return parent::show($id);
+    }
+
+}
