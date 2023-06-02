1380c4fc17dcc971fd88084c8ecf70a6ba2a1942
Diego Pereira <san_diego19@hotmail.com>
2023-06-02 02:15:53 -0300

    test: phpUnit settings

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,53 @@
 
 namespace Tests;
 
+use Closure;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\SQLiteBuilder;
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Fluent;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        $this->hotfixSqlite();
+        parent::setUp();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
+
+    public function hotfixSqlite()
+    {
+        \Illuminate\Database\Connection::resolverFor('sqlite',
+            function ($connection, $database, $prefix, $config) {
+                return new class($connection, $database, $prefix, $config)
+                    extends SQLiteConnection {
+                    public function getSchemaBuilder()
+                    {
+                        if ($this->schemaGrammar === null) {
+                            $this->useDefaultSchemaGrammar();
+                        }
+
+                        return new class($this) extends SQLiteBuilder {
+                            protected function createBlueprint($table, Closure $callback = null)
+                            {
+                                return new class($table, $callback) extends Blueprint {
+                                    public function dropForeign($index)
+                                    {
+                                        return new Fluent();
+                                    }
+                                };
+                            }
+                        };
+                    }
+                };
+            });
+    }
 }
