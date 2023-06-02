7a0fa52b108d611862a93bc35468761c79b90c31
Diego Pereira <san_diego19@hotmail.com>
2023-06-02 11:54:24 -0300

    feat: implementing swagger client controller

--- app/Domain/Client/Controllers/ClientController.php
+++ app/Domain/Client/Controllers/ClientController.php
@@ -29,10 +29,57 @@ class ClientController extends Controller
         $this->repository = $repository;
     }
 
+    /**
+     * @OA\Get(
+     * tags={"Client - List All Clients"},
+     * path="/api/clients/list",
+     * summary="List All Clients",
+     * description="List All Clients",
+     * security={
+     * {"bearer":{}}},
+     * @OA\Response(
+     * response=200,
+     * description="Success"
+     * ),
+     * @OA\Response(
+     * response="default",
+     * description="Unidentified error"
+     * )
+     * )
+     */
+
     public function index(Request $request)
     {
         return parent::index($request);
     }
+
+    /**
+     * @OA\Post(
+     * tags={"Client - Create Client"},
+     * path="/api/clients/create",
+     * summary="Create Record",
+     * description="Create Record",
+     * security={
+     * {"bearer":{}}},
+     * @OA\RequestBody(
+     * required = true,
+     * @OA\MediaType(
+     * mediaType="application/json",
+     * @OA\Schema(
+     * example={"name": "1","email": "diego.pereira26@gmail.com","phone": "11 99999-9999","birth_date": "1988-07-26","address": "Test endereço","complement": "Test complemento","neighborhood": "Test Bairro","zip_code": "67145007"},
+     * )
+     * )
+     * ),
+     * @OA\Response(
+     * response=201,
+     * description="Success"
+     * ),
+     * @OA\Response(
+     * response="default",
+     * description="Unidentified error"
+     * )
+     * )
+     */
 
 
     public function store(Request $request)
@@ -40,19 +87,78 @@ class ClientController extends Controller
         return parent::store($request);
     }
 
+        /**
+     * @OA\Put(
+     * tags={"Client - Update Client"},
+     * path="/api/clients/update/{id}",
+     * summary="Update Record",
+     * description="Update Record",
+     * security={
+     * {"bearer": {}}},
+     * @OA\Parameter(
+     * name="id",
+     * description="Id",
+     * required=true,
+     * in="path",
+     * @OA\Schema(
+     * type="integer"
+     * )
+     * ),
+     * @OA\RequestBody(
+     * @OA\MediaType(
+     * mediaType="application/json",
+     * @OA\Schema(
+     * example={"name": "1","email": "diego.pereira26@gmail.com","phone": "11 99999-9999","birth_date": "1988-07-26","address": "Test endereço","complement": "Test complemento","neighborhood": "Test Bairro","zip_code": "67145007"},
+     * )
+     * )
+     * ),
+     * @OA\Response(
+     * response=200,
+     * description="Success",
+     * ),
+     * @OA\Response(
+     * response="default",
+     * description="Unidentified error"
+     * )
+     * )
+     */
+
     public function update(Request $request, int $id)
     {
         return parent::update($request, $id);
     }
 
+    /**
+     * @OA\Delete (
+     * tags={"Client - Delete Client"},
+     * path="/api/clients/delete/{id}",
+     * summary="Delete Record",
+     * description="Delete Record",
+     * security={
+     * {"bearer": {}}},
+     * @OA\Parameter(
+     * name="id",
+     * description="Id",
+     * required=true,
+     * in="path",
+     * @OA\Schema(
+     * type="integer"
+     * )
+     * ),
+     * @OA\Response(
+     * response=204,
+     * description="Success",
+     * ),
+     * @OA\Response(
+     * response="default",
+     * description="Unidentified error"
+     * )
+     * )
+     */
+
     public function destroy(int $id)
     {
         return parent::destroy($id);
-    }
-
-    public function show($id)
-    {
-        return parent::show($id);
     }
 
 }
